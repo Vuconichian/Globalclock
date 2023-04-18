4ce9d2e9d4f3ef922f4230f1a7b19cb2068e0ae5
Vuconichian <ian.10.vuconich@gmail.com>
2023-04-17 21:13:18 -0300

    probando js

---
+++ index.js
@@ -0,0 +1,27 @@
+function getTimezoneOffset() {
+    var d = new Date();
+    var offset = d.getTimezoneOffset();
+    return offset;
+}
+
+function getLocalTimeFromUTC(utcDate) {
+    var d = new Date(utcDate);
+    var offset = d.getTimezoneOffset();
+    var localTime = new Date(d.getTime() - (offset * 60 * 1000));
+    return localTime;
+}
+
+function displayAllUTCTimes() {
+    var utcDate = new Date();
+    var utcTimesDiv = document.getElementById("utc-times");
+    for (var i = -12; i <= 12; i++) {
+        utcDate.setUTCHours(utcDate.getUTCHours() + i);
+        var localTime = getLocalTimeFromUTC(utcDate);
+        var utcTimeString = "UTC" + (i >= 0 ? "+" : "") + i + ": " + localTime;
+        var utcTimeElement = document.createElement("p");
+        var utcTimeText = document.createTextNode(utcTimeString);
+        utcTimeElement.appendChild(utcTimeText);
+        utcTimesDiv.appendChild(utcTimeElement);
+    }
+}
+displayAllUTCTimes();

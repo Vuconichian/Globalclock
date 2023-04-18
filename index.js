c23da967a695a82f9b4a9f0c208317f9610ad660
Vuconichian <ian.10.vuconich@gmail.com>
2023-04-17 21:41:10 -0300

    casi listo

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+function getTimezoneOffset() {
+    var d = new Date();
+    var offset = d.getTimezoneOffset();
+    return offset;
+}
 
-function displayAllUTCTimes(utcOffset) {
+function getLocalTimeFromUTC(utcDate) {
+    var d = new Date(utcDate);
+    var offset = d.getTimezoneOffset();
+    var localTime = new Date(d.getTime() - (offset * 60 * 1000));
+    return localTime;
+}
+
+function displayAllUTCTimes() {
     var utcDate = new Date();
     var utcTimesDiv = document.getElementById("utc-times");
-    utcTimesDiv.innerHTML = ""; // Vaciar el contenedor antes de actualizarlo
+    utcTimesDiv.innerHTML = "";
     for (var i = -12; i <= 12; i++) {
-        utcDate.setUTCHours(utcDate.getUTCHours() + i + utcOffset);
+        utcDate.setUTCHours(utcDate.getUTCHours() + i);
         var localTime = getLocalTimeFromUTC(utcDate);
         var utcTimeString = "UTC" + (i >= 0 ? "+" : "") + i + ": " + localTime;
         var utcTimeElement = document.createElement("p");
@@ -14,28 +26,34 @@ function displayAllUTCTimes(utcOffset) {
     }
 }
 
-document.getElementById("utc-minus").addEventListener("click", function() {
-    var utcOffset = parseInt(localStorage.getItem("utcOffset"));
-    if (utcOffset > -12) {
-        utcOffset--;
-        localStorage.setItem("utcOffset", utcOffset);
-        displayAllUTCTimes(utcOffset);
-    }
-});
-
-document.getElementById("utc-plus").addEventListener("click", function() {
-    var utcOffset = parseInt(localStorage.getItem("utcOffset"));
-    if (utcOffset < 12) {
-        utcOffset++;
-        localStorage.setItem("utcOffset", utcOffset);
-        displayAllUTCTimes(utcOffset);
-    }
-});
-
-// Obtener la zona horaria almacenada en el localStorage
-var utcOffset = parseInt(localStorage.getItem("utcOffset"));
-if (isNaN(utcOffset)) {
-    utcOffset = 0;
-    localStorage.setItem("utcOffset", utcOffset);
+function updateHour(offset) {
+    var localTime = new Date();
+    var utcTime = new Date(localTime.getTime() + (offset * 60 * 60 * 1000));
+    var hour = utcTime.getUTCHours().toString().padStart(2, "0");
+    var minute = utcTime.getUTCMinutes().toString().padStart(2, "0");
+    var second = utcTime.getUTCSeconds().toString().padStart(2, "0");
+    var timeString = hour + ":" + minute + ":" + second;
+    var timeElement = document.getElementById("local-time");
+    timeElement.innerHTML = timeString;
 }
-displayAllUTCTimes(utcOffset);
+
+function init() {
+    // Mostrar hora local
+    updateHour(getTimezoneOffset()/-60);
+
+    // Mostrar horas en UTC
+    displayAllUTCTimes();
+
+    // Ajustar hora hacia adelante
+    var forwardButton = document.getElementById("forward-button");
+    forwardButton.addEventListener("click", function() {
+        updateHour(getTimezoneOffset()/-60 + 1);
+    });
+
+    // Ajustar hora hacia atrás
+    var backButton = document.getElementById("back-button");
+    backButton.addEventListener("click", function() {
+        updateHour(getTimezoneOffset()/-60 - 1);
+    });
+}
+init();

ae087499337968e4f9ad0312ef45a59f9380b662
Vuconichian <ian.10.vuconich@gmail.com>
2023-04-17 22:01:15 -0300

    probando zonas horarias

--- index.js
+++ index.js
@@ -26,6 +26,34 @@ function displayAllUTCTimes() {
     }
 }
 
+var cityInfo = {
+    "-12": "Eniwetok, Kwajalein",
+    "-11": "Samoa",
+    "-10": "Hawaiian Standard Time",
+    "-9": "Alaska Standard Time",
+    "-8": "Pacific Standard Time",
+    "-7": "Mountain Standard Time",
+    "-6": "Central Standard Time",
+    "-5": "Lima - Peru",
+    "-4": "Atlantic Standard Time",
+    "-3": "Brazilia",
+    "-2": "Mid-Atlantic",
+    "-1": "Azores, Cape Verde",
+    "0": "Greenwich Mean Time",
+    "1": "Central European Time",
+    "2": "Eastern European Time",
+    "3": "Baghdad, Kuwait, Moscow",
+    "4": "Abu Dhabi, Muscat, Baku",
+    "5": "Karachi, Tashkent",
+    "6": "Almaty, Dhaka",
+    "7": "Bangkok, Jakarta",
+    "8": "Hong Kong, Beijing",
+    "9": "Tokyo, Seoul",
+    "10": "Sydney, Melbourne",
+    "11": "Solomon Islands, New Caledonia",
+    "12": "Fiji, Kamchatka"
+};
+
 function updateHour(offset) {
     var localTime = new Date();
     var utcTime = new Date(localTime.getTime() + (offset * 60 * 60 * 1000));
@@ -33,10 +61,17 @@ function updateHour(offset) {
     var minute = utcTime.getUTCMinutes().toString().padStart(2, "0");
     var second = utcTime.getUTCSeconds().toString().padStart(2, "0");
     var timeString = hour + ":" + minute + ":" + second;
-    var timeElement = document.getElementById("utc-times");
+    var timeElement = document.getElementById("local-time");
     timeElement.innerHTML = timeString;
-}
 
+    var cityInfoElement = document.getElementById("city-info");
+    var cityInfoText = cityInfo[hour];
+    if (cityInfoText) {
+        cityInfoElement.innerHTML = cityInfoText;
+    } else {
+        cityInfoElement.innerHTML = "";
+    }
+}
 
 function init() {
     // Mostrar hora local
